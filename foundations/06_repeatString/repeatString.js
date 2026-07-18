5e3a3a8bea5fb992459c3725b775d737e07d0863
carnocentaurus <carnocentaurus@gmail.com>
2026-07-18 12:06:21 +0800

    Repeated strings without using the repeat method

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+function repeatString(string, num) {
+  if (num <0) {
+    return 'ERROR';
+  }
 
-};
+  let repeatedString = '';
+
+  while (num > 0) {
+    repeatedString += string;
+    num --;
+  }
+
+  return repeatedString;
+}
+
+const number = Math.floor(Math.random() * 1000);
+
+repeatString('', 10);
 
 // Do not edit below this line
 module.exports = repeatString;

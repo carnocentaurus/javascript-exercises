f20b90d34fdc95c14256ca9cd577b124de3c7d04
carnocentaurus <carnocentaurus@gmail.com>
2026-07-18 14:17:13 +0800

    Reverse string function using for loop and string at method

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversedString = '';
 
+  for (let i = 1; i <= string.length; i++) {
+    reversedString += string.at(-i);
+  }
+
+  return reversedString;
 };
+
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;

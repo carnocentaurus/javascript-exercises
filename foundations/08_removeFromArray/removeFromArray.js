c9972e3b2da68763953324005830ecfa670a0bdd
carnocentaurus <carnocentaurus@gmail.com>
2026-07-19 14:06:05 +0800

    Removed specified items from array using filter method and rest paramter

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...itemsToRemove) {
+    return array.filter(item => !itemsToRemove.includes(item));
 };
+
+removeFromArray([1, 2, 3], "1", 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;

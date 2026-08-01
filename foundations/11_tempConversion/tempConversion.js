cb46cd5605b1ba5948c357341836e9f273427fa8
carnocentaurus <carnocentaurus@gmail.com>
2026-08-01 13:58:11 +0800

    Temperature convertion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+    const temperatureInCelsius = (temperature - 32) * 5 / 9;
+    return temperatureInCelsius === 0 ? temperatureInCelsius : 
+        Number(temperatureInCelsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+    const temperatureInFahrenheit = (temperature * 9 / 5 + 32);
+    return temperatureInFahrenheit === 0 ? temperatureInFahrenheit : 
+        Number(temperatureInFahrenheit.toFixed(1));
 };
+
+convertToCelsius(-100);
+convertToFahrenheit(-10);
 
 // Do not edit below this line
 module.exports = {

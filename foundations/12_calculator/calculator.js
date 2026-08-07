f5ba771536260ea50c080af98c41a386ba6a2547
carnocentaurus <carnocentaurus@gmail.com>
2026-08-08 06:47:14 +0800

    Implement sum functionality and update tests for the sum method

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -6,8 +6,10 @@ const subtract = function(minuend, subtrahend) {
 	return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+	return arrayOfNumbers.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
 };
 
 const multiply = function() {
@@ -24,6 +26,7 @@ const factorial = function() {
 
 add(2, 6);
 subtract(-8, 7);
+sum([1, 3, 5, 7, 9]);
 
 // Do not edit below this line
 module.exports = {

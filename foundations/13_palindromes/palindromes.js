4534ebf0952f46b091c23352667a5ae24d2f504f
carnocentaurus <carnocentaurus@gmail.com>
2026-08-08 12:10:33 +0800

    Implement palindrome detection functionality and update tests to enable all cases

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const filteredString = string.
+    replaceAll(', ', '').
+    replaceAll('.', '').
+    replaceAll('!', '').
+    replaceAll(' ', '').
+    toLowerCase();
 
+    let reversedString = '';
+
+    for (let i = 1; i <= filteredString.length; i++) {
+      reversedString += filteredString.at(-i);
+    }
+
+    return reversedString === filteredString ? true : false;
 };
+
+palindromes('r3ace3car');
 
 // Do not edit below this line
 module.exports = palindromes;

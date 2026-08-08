e5935aad7821e733a590ba76567df754568f748f
carnocentaurus <carnocentaurus@gmail.com>
2026-08-08 09:25:59 +0800

    Implement factorial functionality and update tests for the factorial method

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -22,8 +22,16 @@ const power = function(base, exponent) {
 	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let numbersInRange = [];
+
+  for (let i = number; i >= 1; i--) {
+    numbersInRange.push(i);
+  }
+
+  return numbersInRange.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
 };
 
 add(2, 6);
@@ -31,6 +39,7 @@ subtract(-8, 7);
 sum([1, 3, 5, 7, 9]);
 multiply([2, 4, 6, 8, 10, 12, 14]);
 power(3, 10);
+factorial(10);
 
 // Do not edit below this line
 module.exports = {

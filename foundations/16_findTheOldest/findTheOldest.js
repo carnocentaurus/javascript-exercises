1caaba53b4e1513c04da0285346ad2cd49bd0697
carnocentaurus <carnocentaurus@gmail.com>
2026-08-09 14:43:11 +0800

    Add HTML file for finding the oldest person in an array of objects

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,39 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+    //yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const date = new Date();
+
+const findTheOldest = function(array) {
+  array.sort((a, b) => {
+    const previousPerson = a.yearOfDeath === undefined ? 
+      date.getFullYear() - a.yearOfBirth :
+      a.yearOfDeath - a.yearOfBirth;
+    const nextPerson = b.yearOfDeath === undefined ? 
+      date.getFullYear() - b.yearOfBirth :
+      b.yearOfDeath - b.yearOfBirth;
+
+    return previousPerson > nextPerson ? -1 : 1;
+  });
+
+  return array[0];
 };
+
+findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;

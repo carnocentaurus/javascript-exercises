925e4ed3e509e4843cb5b508c9f20e4cde48169c
carnocentaurus <carnocentaurus@gmail.com>
2026-08-09 11:35:38 +0800

    Implement getTheTitles function to extract book titles from an array of book objects

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+];
 
+const getTheTitles = function(array) {
+  let values = [];
+
+  array.forEach(book => values.push(book.title));
+
+  return values;
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
